7d80ce0085e8e55bd699bf4792683db139b7b21b
Luis Berenguer <luis01developer@gmail.com>
2021-08-02 21:43:43 +0200

    Update script.js
    There was a buf that dind't load the random texts

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function printMessage(){
-    let el = document.getElementById('random-message');
-    // We have to put the final message below
-    let randomNum = Math.floor(Math.random() * 9 + 1);
-  el.textContent = `This is random message: ${randomNum}`;
-  }
-  
+  let el = document.getElementById('random-message');
+  // We have to put the final message below
+  let randomNum = Math.floor(Math.random() * 9 + 1);
+  el.innerHTML = `This is random message: ${randomNum}`;
+}
 
-  // If we want a record of all the random messages, we change printMessage() for recorgMessges() in HTML
+// If we want a record of all the random messages, we change printMessage() for recorgMessges() in HTML
 
 function recordMessages(){
   let randomNum = Math.floor(Math.random() * 9 + 1);
