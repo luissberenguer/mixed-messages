fcfa7ca15d346cecab614d3baa41edebdf4e5e42
Luis Berenguer <luis01developer@gmail.com>
2021-08-02 18:40:41 +0200

    Add a random number to script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function printMessage(){
     let el = document.getElementById('random-message');
     // We have to put the final message below
-  el.innerHTML = 'This is a new message';
+    let randomNum = Math.floor(Math.random() * 9 + 1);
+  el.innerHTML = `This is random message: ${randomNum}`;
   }
   
