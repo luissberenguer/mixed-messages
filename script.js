be3ac7ed10f3372d3ab6f27889c4631c039ca83f
Luis Berenguer <luis01developer@gmail.com>
2021-08-02 18:59:08 +0200

    Improve printMessage

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function printMessage(){
     let el = document.getElementById('random-message');
     // We have to put the final message below
     let randomNum = Math.floor(Math.random() * 9 + 1);
-  el.innerHTML = `This is random message: ${randomNum}`;
+  el.textContent = `This is random message: ${randomNum}`;
   }
   
